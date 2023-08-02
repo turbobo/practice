6fef8438ab57dff68da39ed54d34e274efa504c3
turbobo <735567435@qq.com>
2023-08-02 14:12:30 +0800

    add: 2023.08.01 快手测开面试：最长不重复子串，注意比较大小的位置

--- src/com/demo/interview/interview2023/kuaishou0801/test1.java
+++ src/com/demo/interview/interview2023/kuaishou0801/test1.java
@@ -7,10 +7,12 @@ import java.util.Set;
  * @Author Jusven
  * @Date 2023/8/1 18:59
  * 最长没有重复的子串
+ * leetcode 3
  */
 public class test1 {
     public static void main(String[] args) {
         System.out.println(getMaxSubString("ABDFAKED"));
+        System.out.println(getMaxSubString2("ABDFAKED"));
     }
     public static String getMaxSubString(String str) {
         if (str.length() == 0) {
@@ -48,6 +50,46 @@ public class test1 {
         } else {
             // 这里加上最大长度即可，不用再加1，因为l位置已经是算一个长度，substring右边是开区间，左边是闭区间 [)
             return str.substring(l, l+max);
+        }
+    }
+
+
+    // 借助set  当右指针遇到重复字符，则依次将左边的字符删除，直到把当前右指针位置的相同元素删除
+    public static String getMaxSubString2(String str) {
+        int max = 0;
+        int left = 0;
+        Set<Character> set = new HashSet<>();
+        int slow = 0;
+        int fast = 0;
+        while (slow<str.length() && fast<str.length()) {
+            // 右指针始终往后移动
+            while (fast<str.length()) {
+                if (set.contains(str.charAt(fast))) {
+                    // 包含右指针元素
+                    set.remove(str.charAt(slow));
+                    slow++;
+                    break;
+                } else {
+                    // 无重复，则把右指针位置元素加入
+                    set.add(str.charAt(fast));
+                    fast++;
+                    // 直接比较set大小即可
+//                    max = Math.max(max, set.size());  //此写法没有错误，但是位置错误，后面max还需要和set大小比较，此处不能先比较！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+                    if (max < set.size()) {
+                        left = slow;
+                        //
+                        max =set.size();
+//                        max = Math.max(max, set.size());  //这里比较
+                    }
+                }
+            }
+
+        }
+
+        if (max == 0) {
+            return "";
+        } else {
+            return str.substring(left, left + max);
         }
     }
 }
